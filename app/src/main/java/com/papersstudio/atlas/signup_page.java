2ea80c174b06c6c012469caed8433d3b71583c4b
Admin <bvkaura@gmail.com>
2022-03-31 12:11:05 +0530

    #2

--- app/src/main/java/com/papersstudio/atlas/signup_page.java
+++ app/src/main/java/com/papersstudio/atlas/signup_page.java
@@ -3,13 +3,24 @@ package com.papersstudio.atlas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
+
+import com.papersstudio.atlas.databinding.ActivitySignupPageBinding;
 
 public class signup_page extends AppCompatActivity {
-
+    private ActivitySignupPageBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup_page);
+        binding=ActivitySignupPageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         this.getSupportActionBar().hide();
+
+        viewfuncs();
+    }
+
+    private void viewfuncs(){
+        String text = "<font color=#707671>Have an account?</font> <font color=#47A36C>Log in</font>";
+        binding.loginTxt.setText(Html.fromHtml(text));
     }
 }
