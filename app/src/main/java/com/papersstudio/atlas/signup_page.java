e741c457cbd3fceb947eba8c9724195a0c408ce2
Admin <bvkaura@gmail.com>
2022-03-31 12:59:25 +0530

    #2

--- app/src/main/java/com/papersstudio/atlas/signup_page.java
+++ app/src/main/java/com/papersstudio/atlas/signup_page.java
@@ -2,13 +2,19 @@ package com.papersstudio.atlas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
+import android.widget.Toast;
 
 import com.papersstudio.atlas.databinding.ActivitySignupPageBinding;
 
 public class signup_page extends AppCompatActivity {
     private ActivitySignupPageBinding binding;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    SharedPreferences saveprefs=getSharedPreferences("userlogged",0);
+    SharedPreferences getprefs=getSharedPreferences("userlogged",0);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,10 +23,49 @@ public class signup_page extends AppCompatActivity {
         this.getSupportActionBar().hide();
 
         viewfuncs();
+        uiEvent();
+    }
+
+    private void uiEvent() {
+        binding.signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(binding.nameField.getEditText().toString().equals(null)){
+                    binding.nameField.setErrorContentDescription("Please enter your name");
+                    Toast.makeText(signup_page.this, "Please enter your name", Toast.LENGTH_SHORT).show();
+                }
+                else if(binding.emailField.getEditText().toString().equals(null)){
+                    binding.emailField.setErrorContentDescription("Please enter your email address");
+                    Toast.makeText(signup_page.this, "Please enter your email address", Toast.LENGTH_SHORT).show();
+                }
+                else if(!binding.emailField.getEditText().toString().matches(emailPattern)){
+                    binding.emailField.setErrorContentDescription("Please enter a valid email address");
+                    Toast.makeText(signup_page.this, "Please enter a valid email address", Toast.LENGTH_SHORT).show();
+                }
+                else if(binding.passowrdField.getEditText().toString().equals(null)){
+                    binding.passowrdField.setErrorContentDescription("Please enter your password");
+                    Toast.makeText(signup_page.this, "Please enter your password", Toast.LENGTH_SHORT).show();
+                }
+
+                else if(binding.passowrdField.getEditText().toString().length()<6){
+                    binding.passowrdField.setErrorContentDescription("Please enter a password of atleast 6 characters");
+                    Toast.makeText(signup_page.this, "Please enter a password of atleast 6 characters", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    binding.progressBar.setVisibility(View.VISIBLE);
+                    binding.signupBtn.setVisibility(View.INVISIBLE);
+                    SharedPreferences.Editor editor=saveprefs.edit();
+                    editor.putString("username",binding.nameField.getEditText().toString());
+                    editor.putString("email",binding.emailField.getEditText().toString());
+                    Toast.makeText(signup_page.this, "Account Created", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     private void viewfuncs(){
         String text = "<font color=#707671>Have an account?</font> <font color=#47A36C>Log in</font>";
         binding.loginTxt.setText(Html.fromHtml(text));
+
     }
 }
