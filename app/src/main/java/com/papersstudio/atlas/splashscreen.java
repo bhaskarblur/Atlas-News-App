7c85b8837d3f16666f9644d3bdfc45eb32229be2
Admin <bvkaura@gmail.com>
2022-03-31 11:15:22 +0530

    #2

--- app/src/main/java/com/papersstudio/atlas/splashscreen.java
+++ app/src/main/java/com/papersstudio/atlas/splashscreen.java
@@ -3,6 +3,7 @@ package com.papersstudio.atlas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -13,7 +14,12 @@ public class splashscreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
         this.getSupportActionBar().hide();
-        
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().setStatusBarColor(getResources().getColor(R.color.green_color, this.getTheme()));
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(getResources().getColor(R.color.green_color));
+        }
+
         mainfunc();
     }
 
