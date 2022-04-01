0cd8f2857796ba2002c3ffd07395d267ba2b3a5f
Admin <bvkaura@gmail.com>
2022-04-01 08:32:08 +0530

    #2

--- app/src/main/java/com/papersstudio/atlas/login_page.java
+++ app/src/main/java/com/papersstudio/atlas/login_page.java
@@ -6,20 +6,23 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Html;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
 import android.view.View;
 import android.widget.Toast;
 
+import com.papersstudio.atlas.databinding.ActivityLoginPageBinding;
 import com.papersstudio.atlas.databinding.ActivitySignupPageBinding;
 
-public class signup_page extends AppCompatActivity {
-    private ActivitySignupPageBinding binding;
+public class login_page extends AppCompatActivity {
+    private ActivityLoginPageBinding binding;
     String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
     SharedPreferences saveprefs;
     SharedPreferences getprefs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding=ActivitySignupPageBinding.inflate(getLayoutInflater());
+        binding=ActivityLoginPageBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         this.getSupportActionBar().hide();
         saveprefs=this.getSharedPreferences("userlogged",0);
@@ -29,10 +32,10 @@ public class signup_page extends AppCompatActivity {
     }
 
     private void uiEvent() {
-        binding.loginTxt.setOnClickListener(new View.OnClickListener() {
+        binding.signinTxt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(signup_page.this,login_page.class));
+                startActivity(new Intent(login_page.this,signup_page.class));
                 finish();
                 overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_right);
             }
@@ -40,48 +43,41 @@ public class signup_page extends AppCompatActivity {
         binding.googleLay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(signup_page.this, "Google Auth is currently unavailable.", Toast.LENGTH_SHORT).show();
+                Toast.makeText(login_page.this, "Google Auth is currently unavailable.", Toast.LENGTH_SHORT).show();
             }
         });
         binding.facebookLay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(signup_page.this, "Facebook Auth is currently unavailable.", Toast.LENGTH_SHORT).show();
+                Toast.makeText(login_page.this, "Facebook Auth is currently unavailable.", Toast.LENGTH_SHORT).show();
             }
         });
-        binding.signupBtn.setOnClickListener(new View.OnClickListener() {
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(binding.name.getText().toString().isEmpty()){
-                    binding.name.setError("Please enter your name");
-                    Toast.makeText(signup_page.this, "Please enter your name", Toast.LENGTH_SHORT).show();
-                }
-                else if(binding.email.getText().toString().isEmpty()){
+
+                if(binding.email.getText().toString().isEmpty()){
                     binding.email.setError("Please enter your email address");
-                    Toast.makeText(signup_page.this, "Please enter your email address", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(login_page.this, "Please enter your email address", Toast.LENGTH_SHORT).show();
                 }
                 else if(!binding.email.getText().toString().trim().matches(emailPattern)){
                     binding.email.setError("Please enter a valid email address");
-                    Toast.makeText(signup_page.this, "Please enter a valid email address", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(login_page.this, "Please enter a valid email address", Toast.LENGTH_SHORT).show();
                 }
                 else if(binding.password.getText().toString().equals(null)){
                     binding.password.setError("Please enter your password");
-                    Toast.makeText(signup_page.this, "Please enter your password", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(login_page.this, "Please enter your password", Toast.LENGTH_SHORT).show();
                 }
 
-                else if(binding.password.getText().toString().length()<6){
-                    binding.password.setError("Please enter a password of atleast 6 characters");
-                    Toast.makeText(signup_page.this, "Please enter a password of atleast 6 characters", Toast.LENGTH_SHORT).show();
-                }
                 else{
                     binding.progressBar.setVisibility(View.VISIBLE);
-                    binding.signupBtn.setVisibility(View.INVISIBLE);
+                    binding.loginBtn.setVisibility(View.INVISIBLE);
                     SharedPreferences.Editor editor=saveprefs.edit();
                     editor.putString("logged","yes");
-                    editor.putString("username",binding.name.getText().toString());
+                   // editor.putString("username",binding.name.getText().toString());
                     editor.putString("email",binding.email.getText().toString().trim());
                     editor.commit();
-                    Toast.makeText(signup_page.this, "Account created!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(login_page.this, "Welcome back!", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -92,13 +88,17 @@ public class signup_page extends AppCompatActivity {
         if(logged.equals("yes")) {
             //continue to home page
         }
-        String text = "<font color=#707671>Have an account?</font> <font color=#47A36C>Log in</font>";
-        binding.loginTxt.setText(Html.fromHtml(text));
+        String text = "<font color=#707671>New to Atlas?</font> <font color=#47A36C>Sign up</font>";
+        binding.signinTxt.setText(Html.fromHtml(text));
+        SpannableString content = new SpannableString("Need help logging?");
+        content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
+        binding.loginhelpTxt.setText(content);
 
     }
+
     @Override
     public void finish() {
         super.finish();
-        overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_left);
+        overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_right);
     }
 }
