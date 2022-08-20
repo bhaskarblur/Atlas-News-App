912b5149374b4a542b4034b6640039509adb13f6
Forest Defenders Club NFTs <bvkaura@gmail.com>
2022-08-20 17:25:47 +0530

    added single category and news activity

---
+++ app/src/main/java/com/papersstudio/atlas/single_news_Activity.java
@@ -0,0 +1,14 @@
+package com.papersstudio.atlas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class single_news_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_news);
+    }
+}
